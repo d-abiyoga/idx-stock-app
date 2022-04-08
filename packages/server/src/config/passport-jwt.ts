eaf59e234e16096831fc250225d7465b48664d26
d-abiyoga <dimas.abiyoga169@gmail.com>
2022-04-08 21:32:56 +0700

    feat: add passport-jwt strategy

---
+++ packages/server/src/config/passport-jwt.ts
@@ -0,0 +1,27 @@
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+import { User } from '../models/user';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const secretOrKey = isProduction ? process.env.JWT_SECRET_PROD : process.env.JWT_SECRET_DEV;
+
+// JWT strategy
+export const jwtLogin = new Strategy(
+  {
+    jwtFromRequest: ExtractJwt.fromHeader('x-auth-token'),
+    secretOrKey,
+  },
+  async (payload, done) => {
+    try {
+      const user = await User.findById(payload.id);
+
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    } catch (err) {
+      done(err, false);
+    }
+  },
+);
